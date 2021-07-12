745b51b0e7d67f7a0b1c5ae28943f98057073058
Addison Simon <addisonsimon@Addisons-MacBook-Air.local>
2021-07-12 13:22:03 -0500

    commit

--- src/lab14.cpp
+++ src/lab14.cpp
@@ -18,43 +18,56 @@ void setup();
 void loop();
 #line 10 "/Users/addisonsimon/lab14/src/lab14.ino"
 SYSTEM_THREAD(ENABLED);
+
+OledWingAdafruit display;
 void callback(char* topic, byte* payload, unsigned int length) {
-    Serial.print("Received message on testTopic/wbk: \n");
-    for (int i = 0; i < length; i++) {
-      Serial.print((char)payload[i]);
-    }
     char p[length + 1];
-    memcpy(p, payload, length);
-    p[length] = NULL;
-    if (!strcmp(p, "1")) {
-      digitalWrite(D7, HIGH);
-    }
-    if (!strcmp(p, "0")) {
-      digitalWrite(D7, LOW);
-    }
-    Serial.println();
+  memcpy(p, payload, length);
+  p[length] = NULL;
+  Serial.printf("%s",p);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println(p);
+  display.display();
+  delay(1000);
+  Serial.println();
+  // if press 1 it turns led on, 0 turns it off
+  if(p[0] == '1'){
+    digitalWrite(D7,HIGH);
+    Serial.println("ON");
+  }
+  if(p[0] == '0'){
+    digitalWrite(D7,LOW);
+    Serial.println("OFF");
+  }
 }
 MQTT client("lab.thewcl.com", 1883, callback);
 String message;
 
-OledWingAdafruit display;
 
 BLYNK_WRITE(V1) {
    message = param.asString();
    client.publish("testTopic/wbk", message);
+   if(message == "1"){digitalWrite(D7, HIGH);}
+   if(message == "0"){digitalWrite(D7, LOW);}
+   display.clearDisplay();
+   display.println(message);
+   display.display();
 }
 // setup() runs once, when the device is first turned on.
 void setup() {
   // Put initialization like pinMode and begin functions here.
-  client.subscribe("testTopic/wbk");
   display.setup();
   display.clearDisplay();
   display.display();
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0,0);
-  pinMode(D4, OUTPUT);
-  Blynk.begin("MViijk-riOK2QXDW7o1k0yJkajixIVMq", IPAddress(167, 172, 234, 162), 9090);
+  Serial.begin(9600);
+  pinMode(D7, OUTPUT);
+  Blynk.begin("FQI3d79SCWEHkMActo_VZLsDHmLrUVPU", IPAddress(167, 172, 234, 162), 9090);
 }
 
 // loop() runs over and over again, as quickly as it can execute.
@@ -66,6 +79,7 @@ void loop() {
     client.loop();
   } else {
     client.connect(System.deviceID());
+    client.subscribe("something");
   }
   if(display.pressedA()){
     client.publish("testTopic/wbk", "A was pressed");
